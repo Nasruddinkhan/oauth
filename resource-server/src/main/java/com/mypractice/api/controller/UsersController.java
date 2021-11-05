1b27e4eb3c6b35ba7297cb8574da826076762636
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-11-06 00:34:29 +0530

    method level security like post and pre authorized

--- resource-server/src/main/java/com/mypractice/api/controller/UsersController.java
+++ resource-server/src/main/java/com/mypractice/api/controller/UsersController.java
@@ -2,9 +2,18 @@ package com.mypractice.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mypractice.api.dto.UserDto;
 
 @RestController
 @RequestMapping("/users")
@@ -17,4 +26,18 @@ public class UsersController {
 		return "Working on port: " + env.getProperty("local.server.port");
 	}
 
-}
+	// @PreAuthorize("#id == #jwt.subject")
+	@PreAuthorize("hasAuthority('ROLE_developer') or #id == #jwt.subject")
+	// @PreAuthorize("hasRole('developer')")
+	// @Secured("ROLE_user")
+	@DeleteMapping("/{id}")
+	public String deleteUserById(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return "delete user by : " + id + " and jwt subject is " + jwt.getSubject();
+	}
+
+	@PostAuthorize("hasAuthority('ROLE_developer') or returnObject.userId == #jwt.subject")
+	@GetMapping("/{id}")
+	public UserDto getUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+		return new UserDto("Nasruddin", "Khan", "1d891fc9-746b-4de1-8c5a-5827687b579");
+	}
+} 
--- resource-server/src/main/java/com/mypractice/api/security/WebSecurity.java
+++ resource-server/src/main/java/com/mypractice/api/security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.mypractice.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 	private final KeycloakRoleConverter keyCloackConverter;
 
