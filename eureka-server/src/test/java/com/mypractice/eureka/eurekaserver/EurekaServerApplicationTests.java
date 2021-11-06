ce2d854dd7be67d8504808798d3df648e56cfbbb
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-11-07 00:58:52 +0530

    setup the eureka server

---
+++ eureka-server/src/test/java/com/mypractice/eureka/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypractice.eureka.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
