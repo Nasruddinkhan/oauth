53a220f0259ac2a0997e62cacf096ed8e84abdeb
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-11-07 22:41:37 +0530

    add acess token

--- api-mvc-client/src/main/java/com/mypractice/api/client/controller/ApiController.java
+++ api-mvc-client/src/main/java/com/mypractice/api/client/controller/ApiController.java
@@ -3,7 +3,14 @@ package com.mypractice.api.client.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
@@ -15,6 +22,9 @@ import com.mypractice.api.client.dto.DepartmentDto;
 @Controller
 public class ApiController {
 
+	@Autowired
+	OAuth2AuthorizedClientService  oAuth2AuthorizedClientService;
+	
 	@GetMapping("/department")
 	public String findAllDepartments(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
 
@@ -23,6 +33,21 @@ public class ApiController {
 		String idTokenValue = idToken.getTokenValue();
 		System.out.println("idTokenValue = " + idTokenValue);
 
+		List<DepartmentDto> department = Arrays.asList(
+				DepartmentDto.builder().departmentId("IT-DPT").departmentName("IT-DEPARTMENT").port("").build(),
+				DepartmentDto.builder().departmentId("MGR-DPT").departmentName("MANAGER-DEPARTMENT").port("").build());
+		model.addAttribute("department", department);
+		return "department";
+	}
+
+	@GetMapping("/department/jwtAccessToken")
+	public String findAllDepartmentsJwtAccessToken(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		OAuth2AuthenticationToken auth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+		OAuth2AuthorizedClient authorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(auth2AuthenticationToken.getAuthorizedClientRegistrationId(), auth2AuthenticationToken.getName());
+	    String jwtAccessTOken =	authorizedClient.getAccessToken().getTokenValue();
+	    System.out.println("jwtAccessTOken "+jwtAccessTOken);
 		List<DepartmentDto> department = Arrays.asList(
 				DepartmentDto.builder().departmentId("IT-DPT").departmentName("IT-DEPARTMENT").port("").build(),
 				DepartmentDto.builder().departmentId("MGR-DPT").departmentName("MANAGER-DEPARTMENT").port("").build());
