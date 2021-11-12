56773257c37747465eefbcc82af67456574c9f28
Nasruddinkhan <nasruddinkhan44@gmail.com>
2021-11-12 22:54:44 +0530

    add oauth web client

---
+++ sb-social-web-app/src/main/java/com/mypractice/sb/social/SbSocialWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.mypractice.sb.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSocialWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSocialWebAppApplication.class, args);
+	}
+
+}
